0467986db48f355bec6df54ed3631281af5756bd
Alexander <staridoxberyl2005@gmail.com>
2026-08-03 17:28:47 -0700

    Added Emotions

--- emotionDectector.py
+++ emotionDectector.py
@@ -1,4 +1,39 @@
 from transformers import pipeline
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
-emotionDectector = pipeline("sentiment-analysis", model="j-hartmann/emotion-english-distilroberta-base")
-print(emotionDectector("It hurts to know that no matter how much I wanted us to work, some things are just not meant to be"))
+app = FastAPI()
+
+#Allow your React app to communicate with Python
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173",
+                    "http://127.0.0.1:5173",
+                    "http://192.168.56.1:5173",],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+emotionDectector = pipeline("sentiment-analysis", model="SamLowe/roberta-base-go_emotions")
+
+#Defines the expected JSON FORMAT
+class Message(BaseModel):
+    text: str
+    
+#My message
+def myMessage(thisMessage):
+    return emotionDectector(thisMessage)
+
+#Endpoint that React will call
+@app.post("/analyze")
+def analyze(message: Message):
+    #Get the user's text
+    user_text = myMessage(message.text)
+    emotion = []
+    for user in user_text:
+        emotion.append(user["label"])
+        
+    return {"emotion": emotion}
+    
+
--- open-screen/src/App.tsx
+++ open-screen/src/App.tsx
@@ -5,15 +5,59 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+  //Sends a message
+  const [message, setMessage] = useState("");
+  //Get message
+  const [result, setResult] = useState("");
+  //Loading screen
+  const [loading, setLoading] = useState(false);
+  //Function 
+  async function sendMessage()
+  {
+    try
+    {
+      //Sets the loading
+      setLoading(true);
+      //Sends a response
+      const response = await fetch(
+        "http://192.168.56.1:8000/analyze",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type":"application/json"
+          },
+          body: JSON.stringify({
+            text: message,
+          }),
+        }
+      );
 
+      //Response is an error
+      if(!response.ok)
+      {
+        throw new Error("Python server returned an error");
+      }
+
+      //Gets the data
+      const data = await response.json();
+      setResult(data.emotion[0]);
+      setLoading(false);
+    }
+    catch(error)
+    {
+      console.error(error);
+    }
+  }
   return (
     <>
       <h1 className="title">STOP!, before you send!</h1>
       <h2 className="instruction">Type under here before you send any message</h2>
+      <h2>{loading ? "Loading" : ""}</h2>
       <div className="inputArea">
-          <textarea className='message'></textarea>
+          <textarea className='message' onChange={(e)=> setMessage(e.target.value)}></textarea>
           <br/>
-          <button className='enter'>Enter</button>
+          <button className='enter' onClick={()=>sendMessage()}>Enter</button>
+          <p>{result}</p>
       </div>
     </>
   )
